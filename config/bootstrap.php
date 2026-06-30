d8b34611957760effc57f18b38d62257bcb4943d
Meet Dobariya <mdobariya3606@gmail.com>
2026-06-30 10:30:41 +0530

    logged errors, user cant see exceptions

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,11 +1,24 @@
 <?php
-
-date_default_timezone_set('Asia/Kolkata');
 use Dotenv\Dotenv;
-
+date_default_timezone_set('Asia/Kolkata');
 require __DIR__ . '/../vendor/autoload.php';
+
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
 $dotenv->load();
+
+ini_set('display_error', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/../log/error.log');
+
+error_reporting(E_ALL);
+
+set_exception_handler(function(Throwable $e) {
+    error_log($e);
+    http_response_code(500);
+    echo '<h3>Something went wrong, Please try again later.</h3>';
+
+    // echo "<script>console.log($e)</script>";
+});
 
 require __DIR__ . '/conn.php';
 require __DIR__ . '/email.php';
--- config/email.php
+++ config/email.php
@@ -4,18 +4,23 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$mail = new PHPMailer(true);
-$mail->isSMTP();
-$mail->Host = $_ENV['MAIL_HOST'];
+try {
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = $_ENV['MAIL_HOST'];
+    
+    $mail->SMTPAuth = true;
+    $mail->Username = $_ENV['MAIL_USERNAME'];
+    $mail->Password = $_ENV['MAIL_PASSWORD'];
+    
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+    
+    $mail->setFrom($_ENV['MAIL_FROM'], 'Document Access Management System');
+    $mail->addAddress($_ENV['MAIL_TO']);
+} catch (Exception $e) {
+    error_log($e);
+}
 
-$mail->SMTPAuth = true;
-$mail->Username = $_ENV['MAIL_USERNAME'];
-$mail->Password = $_ENV['MAIL_PASSWORD'];
-
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-
-$mail->setFrom($_ENV['MAIL_FROM'], 'Document Access Management System');
-$mail->addAddress($_ENV['MAIL_TO']);
 
 ?>
